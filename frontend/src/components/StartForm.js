93613763a4a9b6e27a50a9778459a12606f9bfec
Daniel Rodriguez <danielro8@gmail.com>
2020-08-15 20:34:42 -0300

    Add start form for initializing values

--- frontend/src/components/StartForm.js
+++ frontend/src/components/StartForm.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import '../css/StartForm.css'
 
 const StartForm = () => {
-    return <div>3123123</div>
+    const isNumberKey = (evt) => { 
+        var charCode = (evt.which) ? evt.which : evt.keyCode 
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) 
+            return false; 
+        return true; 
+    } 
+    return (
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Rows</label>
+                <input type="number" class="form-control" placeholder="Enter the number of rows" onkeydown="if(event.key==='.'){event.preventDefault();}"  oninput="event.target.value = event.target.value.replace(/[^0-9]*/g,'');" />
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Cols</label>
+                <input type="number" class="form-control" placeholder="Enter the number of cols" />
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Mines</label>
+                <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Enter the number of mines" />
+            </div>
+            <button type="submit" class="btn btn-primary">Start</button>
+        </form>
+    )
 }
 
 export default StartForm;
