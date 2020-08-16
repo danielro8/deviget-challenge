8fed16e62b967c1c0603da5d78f819d61e515e35
Daniel Rodriguez <danielro8@gmail.com>
2020-08-15 22:55:23 -0300

    Map class and helpers defined, actions and reducers updated

---
+++ frontend/src/components/Map.js
@@ -0,0 +1,47 @@
+// @flow
+
+import React from "react";
+import Cell from "./Cell";
+import {
+    nestedArray,
+    populateNestedArray,
+    valsAdjacentCounts
+} from "../helpers";
+
+import { useSelector, useDispatch } from 'react-redux'
+
+
+const Map = () => {
+    const rows = useSelector(state => state.game.rows)
+    const cols = useSelector(state => state.game.cols)
+    const bombCount = useSelector(state => state.game.bombCount)
+    const map = useSelector(state => state.game.map)
+    const cellsClicked = useSelector(state => state.game.cellsClicked)
+  
+    return (
+        <div>
+            <table>
+                <tbody>
+                    {map.map((item, row) => {
+                        return (
+                            <tr key={row} className="mapRow">
+                                {item.map((subitem, col) => {
+                                    return (
+                                        <Cell
+                                            key={col}
+                                            row={row}
+                                            column={col}
+                                            value={subitem}
+                                        />
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Map;
