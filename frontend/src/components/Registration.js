1436170ca234da0c2aca507bd0e1c9cfce172927
Daniel Rodriguez <danielro8@gmail.com>
2020-08-17 09:28:11 -0300

    mongo user model

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -9,25 +9,19 @@ const Registration = () => {
             </div>
 
             <div class="form-content">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
+                <div class="form-group">
                             <input type="email" class="form-control" placeholder="email *" value=""/>
                         </div>
-                    </div>
-                    <div class="col-md-6">
                         <div class="form-group">
                             <input type="text" class="form-control" placeholder="Your Password *" value=""/>
                         </div>
                         <div class="form-group">
                             <input type="text" class="form-control" placeholder="Confirm Password *" value=""/>
                         </div>
-                    </div>
                 </div>
-                <button type="button" class="btnSubmit">Submit</button>
+                <button type="button" class="btn btn-primary btn-block">Submit</button>
             </div>
         </div>
-    </div>
     );
   }
   
